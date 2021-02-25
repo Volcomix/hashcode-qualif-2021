ea6e2e1fd7f9b04b76c9b20866e724b72b6f5ca3
Sébastien Jalliffier Verne <s.jalliffierverne@criteo.com>
2021-02-25 21:14:34 +0100

    Move cars between streets

--- src/simulation.ts
+++ src/simulation.ts
@@ -1,4 +1,5 @@
 import {
+  Car,
   Dataset,
   Intersection,
   Schedule,
@@ -17,17 +18,24 @@ function runSchedule(dataset: Dataset, submission: Submission) {
   const secondsToNextScheduleItem = new Map<Intersection, number>(
     submission.schedules.map((schedule) => ([schedule.intersection, 0])),
   );
+  const streetIdxs = new Map<Car, number>(
+    dataset.cars.map((car) => ([car, 0])),
+  );
+  const secondsToNextStreet = new Map<Car, number>(
+    dataset.cars.map((car) => ([car, 0])),
+  );
+
   for (let second = 0; second < dataset.duration; second++) {
     for (const intersection of dataset.intersections) {
       const schedule = schedulesByIntersection.get(intersection);
       if (!schedule) {
         continue;
       }
-      let scheduleItemIdx = scheduleItemIdxs.get(intersection)!;
       const secondToNextScheduleItem = secondsToNextScheduleItem.get(
         intersection,
       )!;
       if (secondToNextScheduleItem === second) {
+        let scheduleItemIdx = scheduleItemIdxs.get(intersection)!;
         scheduleItemIdx = (scheduleItemIdx + 1) % schedule.items.length;
         scheduleItemIdxs.set(intersection, scheduleItemIdx);
         const scheduleItem = schedule.items[scheduleItemIdx];
@@ -36,6 +44,19 @@ function runSchedule(dataset: Dataset, submission: Submission) {
           intersection,
           second + scheduleItem.duration,
         );
+      }
+    }
+    for (const car of dataset.cars) {
+      const secondToNextStreet = secondsToNextStreet.get(car)!;
+      if (secondToNextStreet === second) {
+        let streetIdx = streetIdxs.get(car)!;
+        streetIdx++;
+        if (streetIdx === car.paths.length) {
+          // TODO Score and remove car
+        }
+        streetIdxs.set(car, streetIdx);
+        const street = car.paths[streetIdx];
+        secondsToNextStreet.set(car, second + street.duration);
       }
     }
   }
