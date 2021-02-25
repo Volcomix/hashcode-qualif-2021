3c9b7b9c4e9c019f98949a526a6726d54b6a547d
vincent-tr <vincent@stoic.com>
2021-02-25 19:52:58 +0100

    wip dataset

--- src/dataset.ts
+++ src/dataset.ts
@@ -1,13 +1,57 @@
 import { trimLines } from "./helpers/string.ts";
+import { Car, Dataset, Intersection, Street } from "./model.ts";
 
 export async function readDataset(inputFilePath: string): Promise<Dataset> {
   const input = await Deno.readTextFile(inputFilePath);
-  const [teamsLine, ...pizzaLines] = trimLines(input.split("\n"));
-  ingredientMap.clear();
+
+  const lines = trimLines(input.split("\n"));
+  let lineIndex = 0;
+  const nextLine = () => lines[lineIndex++];
+
+  const [duration, intersectionsCount, streetsCount, carsCount, bonusPerCar] =
+    parseNumbers(nextLine());
+
+  const intersections = new Array<Intersection>(intersectionsCount);
+  for (let i = 0; i < intersectionsCount; ++i) {
+    intersections[i] = {
+      id: i,
+      arrivals: [],
+      departures: [],
+    };
+  }
+
+  const streets = new Array<Street>(streetsCount);
+  const streetsByName = new Map<string, Street>();
+
+  for (let i = 0; i < streetsCount; ++i) {
+    const [bstr, estr, name, durationstr] = nextLine().split(" ");
+    const from = intersections[Number(bstr)];
+    const to = intersections[Number(estr)];
+    const duration = Number(durationstr);
+
+    const street: Street = { name, from, to, duration };
+    streets[i] = street;
+    from.departures.push(street);
+    to.arrivals.push(street);
+    streetsByName.set(name, street);
+  }
+
+  const cars = new Array<Car>(carsCount);
+  for (let i = 0; i < carsCount; ++i) {
+    const [pstr, ...streetNames] = nextLine().split(" ");
+    const car: Car = {
+      paths: streetNames.map((name) => streetsByName.get(name)!),
+    };
+    cars[i] = car;
+  }
+
   return {
     name: inputFilePath.split("/").pop()!,
-    teams: parseTeams(teamsLine),
-    pizzas: pizzaLines.map(parsePizza),
+    duration,
+    streets,
+    intersections,
+    cars,
+    bonusPerCar,
   };
 }
 
@@ -70,4 +114,8 @@ function parsePizza(line: string, pizzaId: number): Pizza {
       return ingredient;
     }),
   };
+}
+
+function parseNumbers(line: string) {
+  return line.split(" ").map(Number);
 }
